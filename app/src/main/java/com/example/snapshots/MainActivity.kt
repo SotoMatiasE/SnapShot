979f8ce48b5787bd1cf6a3b8e2c37f11902a2046
Matias <matias_r12@hotmail.com>
2024-05-31 10:30:33 -0300

    Activacion de Fragments

--- app/src/main/java/com/example/snapshots/MainActivity.kt
+++ app/src/main/java/com/example/snapshots/MainActivity.kt
@@ -6,10 +6,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.snapshots.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mBinding : ActivityMainBinding
+
+    private lateinit var mActiveFragment: Fragment
+    private lateinit var mFragmentManager: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +26,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupButtonNav(){
-        val fragmentManager = supportFragmentManager
-        fragmentManager.beginTransaction().add(R.id.hostFragment, HomeFragment()).commit()
+        /*Utiliza mFragmentManager para iniciar una transacción de fragmentos con beginTransaction().
+        * esta función configura la navegación de botones inicializando el administrador
+        * de fragmentos y añadiendo un HomeFragment a un contenedor específico en la interfaz de usuario*/
+        mFragmentManager = supportFragmentManager
+
+        //instanciar los fragmentos()
+        val homeFragment = HomeFragment()
+        val addFragment = AddFragment()
+        val profileFragment = ProfileFragment()
+
+        //seteo ActivFragment
+        mActiveFragment = homeFragment
+
+        //ordenamos los fragemnts para que home sea el primero en mostrar
+        /*en este paso añadimos ProfileFragment y con .hint() lo ocultamos*/
+        mFragmentManager.beginTransaction().add(R.id.hostFragment,
+            profileFragment, ProfileFragment::class.java.name)
+            .hide(profileFragment)
+            .commit()
+
+        mFragmentManager.beginTransaction().add(R.id.hostFragment,
+            addFragment, AddFragment::class.java.name)
+            .hide(addFragment)
+            .commit()
+
+        //como queremos que HomeFragment se vea del inicio sacamos .hide
+        mFragmentManager.beginTransaction().add(R.id.hostFragment,
+            homeFragment, HomeFragment::class.java.name)
+            .commit()
+
     }
 }
