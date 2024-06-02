bf62a1b2c1da49ba0edb5fed17d5c4c1c4f29a9b
Matias <matias_r12@hotmail.com>
2024-06-02 02:51:44 -0300

    cambios y subida en tiempo real

--- app/src/main/java/com/example/snapshots/AddFragment.kt
+++ app/src/main/java/com/example/snapshots/AddFragment.kt
@@ -10,6 +10,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.snapshots.databinding.FragmentAddBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class AddFragment : Fragment() {
     private lateinit var mBinding: FragmentAddBinding
@@ -18,6 +23,11 @@ class AddFragment : Fragment() {
 
     private var mPhotoSelectedUri: Uri? = null
 
+    private val PATH_SNAPSHOT = "snapshots" //para crear una carpeta en el servido y administrar las imagenes
+
+    private lateinit var mStoreageReference: StorageReference
+
+    private lateinit var mDatabaseReference: DatabaseReference//extrar url
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,11 +47,14 @@ class AddFragment : Fragment() {
         mBinding.btnPost.setOnClickListener {
             //metodo
             postSnapshot()
-
-            mBinding.btnSelect.setOnClickListener {
-                openGalery()
-            }
         }
+        mBinding.btnSelect.setOnClickListener {
+            openGalery()
+        }
+
+        mStoreageReference = FirebaseStorage.getInstance().reference
+        //inicialisamos db
+        mDatabaseReference = FirebaseDatabase.getInstance().reference.child(PATH_SNAPSHOT)
     }
 
     private fun openGalery() {//ABRE LA GALERIA
@@ -51,7 +64,45 @@ class AddFragment : Fragment() {
     }
 
     private fun postSnapshot() {
+        //subir imagen a storage
+        mBinding.progressBar.visibility = View.VISIBLE
 
+        val  key = mDatabaseReference.push().key!!
+        val storageReference = mStoreageReference.child(PATH_SNAPSHOT).child("my_photo")
+        if (mPhotoSelectedUri != null){
+            storageReference.putFile(mPhotoSelectedUri!!)
+            .addOnProgressListener {
+                //pintamos el progres barr que se pinte mientra sube la img
+                val progress = (100 * it.bytesTransferred/it.totalByteCount).toDouble()//calcula el porsentaje de byts transferidos
+                mBinding.progressBar.progress = progress.toInt()
+                mBinding.tvMessage.text = "$progress%"
+            }
+            .addOnCompleteListener{
+                //que hace cuando se completa la subida
+                mBinding.progressBar.visibility = View.INVISIBLE
+            }
+            .addOnSuccessListener {
+                //salvar url
+                //extraer URL
+                it.storage.downloadUrl.addOnSuccessListener {
+                    //solo se guarda si se extrajo la url con exito
+                    //it.toString seria la URL
+                    saveSnapshot(key, it.toString(), mBinding.edTitle.text.toString().trim())
+                    mBinding.tilTitle.visibility = View.GONE
+                    mBinding.tvMessage.text = getString(R.string.post_message_title)
+                }
+                Snackbar.make(mBinding.root, "Foto Publicada", Snackbar.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener{
+                Snackbar.make(mBinding.root, "Error al subir la foto", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun saveSnapshot(key: String, url: String, title: String){
+        //salvar nueva url dentro de elemento tipo snapshot
+        val snapshot = SnapShot(title = title, photoUrl = url)
+        mDatabaseReference.child(key).setValue(snapshot)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
